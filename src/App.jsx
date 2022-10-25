62f595c271b4d18981ea14c28906aee2ad7ff0c5
Caio Veiga <ccaioveiga@gmail.com>
2022-10-25 15:54:56 -0300

    Inicio da construcao do ToDo List
    Criacao de componente que exibe as tarefas
    
    Adicao de uma tarefa
    
    Criacao do componente Buttom
    
    Adicao de uma tarefa
    
    Conclusao de uma tarefa

---
+++ src/App.jsx
@@ -0,0 +1,49 @@
+import React, { useState } from "react";
+import { v4 as uuidv4 } from "uuid";
+
+import "./App.css";
+import AddTask from "./components/AddTask";
+import Tasks from "./components/Tasks";
+
+const App = () => {
+  const [tasks, setTasks] = useState([
+    { 
+      id: 1,
+      title: "Estudar", 
+      completed: false 
+    },
+    { 
+      id: 2, 
+      title: "Exercitar", 
+      completed: false 
+    },
+  ]);
+
+  const handleTaskClick = (taskId) => {
+    const newTasks = tasks.map((task)  => {
+      if (task.id === taskId) return { ...task, completed: !task.completed }
+      return task;
+    });
+    setTasks(newTasks)
+  };
+
+  const handleTaskAddition = (taskTitle) => {
+    const newTasks = [...tasks, {
+      title: taskTitle,
+      id: uuidv4(),
+      completed: false,
+    }]
+    setTasks(newTasks);
+  }
+
+  return (
+    <>
+      <div className="container">
+        <AddTask handleTaskAddition={handleTaskAddition}/>
+        <Tasks tasks={tasks} handleTaskClick={handleTaskClick}/>
+      </div>
+    </>
+  );
+};
+
+export default App;
